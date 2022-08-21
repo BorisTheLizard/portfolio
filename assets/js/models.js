2cb45cb0114b5f16c84d80fe01a0e49ecaffe6f7
BorisTheLizard <99308714+BorisTheLizard@users.noreply.github.com>
2022-08-21 14:22:51 -0700

    shader/renderer changes
    use new shotgunModel6 it has the best properties for three.js renderer

--- assets/js/models.js
+++ assets/js/models.js
@@ -13,14 +13,19 @@ animate();
 
 function init() {
   const container = document.getElementsByClassName("slider--item-image")[1];
-  camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 2000 );
+  camera = new THREE.PerspectiveCamera(
+    45,
+    window.innerWidth / window.innerHeight,
+    1,
+    4000
+  );
   camera.position.set(250, 250, 250);
 
   scene = new THREE.Scene();
   //scene.background = new THREE.Color( 0xa0a0a0 );
   const hemiLight = new THREE.HemisphereLight(0xffffff, 0x444444);
   hemiLight.position.set(0, 200, 0);
-  scene.add(hemiLight);
+  //scene.add(hemiLight);
 
   const dirLight = new THREE.DirectionalLight(0xffffff);
   dirLight.position.set(0, 200, 100);
@@ -29,31 +34,38 @@ function init() {
   dirLight.shadow.camera.bottom = -100;
   dirLight.shadow.camera.left = -120;
   dirLight.shadow.camera.right = 120;
-  scene.add(dirLight);
+  //scene.add(dirLight);
 
-   //scene.add( new THREE.CameraHelper( dirLight.shadow.camera ) );
+  //scene.add( new THREE.CameraHelper( dirLight.shadow.camera ) );
 
-  // ground 
+  // ground
   const mesh = new THREE.Mesh(
     new THREE.PlaneGeometry(2000, 2000),
-    new THREE.MeshPhongMaterial({  color: 0x999999, depthWrite: false })
+    new THREE.MeshPhongMaterial({
+      color: 0x999999,
+      //envMap: envMap,
+      specular: 0x050505,
+      shininess: 50,
+      depthWrite: false,
+    })
   );
   mesh.rotation.x = -Math.PI / 2;
   mesh.receiveShadow = true;
-//scene.add(mesh);
+  //scene.add(mesh);
 
   const grid = new THREE.GridHelper(2000, 20, 0x000000, 0x000000);
   grid.material.opacity = 0.2;
   grid.material.transparent = true;
+
   //scene.add(grid);
 
   // model
   const loader = new FBXLoader();
-  loader.load("assets/models/Samba Dancing.fbx", function (object) {
-    mixer = new THREE.AnimationMixer(object);
+  loader.load("assets/models/ShotgunModel6.fbx", function (object) {
+    //mixer = new THREE.AnimationMixer(object);
 
-    const action = mixer.clipAction(object.animations[0]);
-    action.play();
+    //const action = mixer.clipAction(object.animations[0]);
+    //action.play();
 
     object.traverse(function (child) {
       if (child.isMesh) {
@@ -65,12 +77,13 @@ function init() {
     scene.add(object);
   });
 
+  //renderer.roughnessMap = 0;
   renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
   renderer.setClearColor(0x000000, 0);
   renderer.setPixelRatio(window.devicePixelRatio);
   //renderer.setSize(document.getElementsByClassName("slider--item slider--item-active")[0].offsetWidth, document.getElementsByClassName("slider--item slider--item-active")[0].offsetHeight);
-  renderer.setSize(window.innerWidth,window.innerHeight);
-  renderer.shadowMap.enabled = true;
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  //renderer.shadowMap.enabled = true;
   container.appendChild(renderer.domElement);
 
   const controls = new OrbitControls(camera, renderer.domElement);
